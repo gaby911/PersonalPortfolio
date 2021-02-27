7b4bb88c3086e21f127d04d26e4b2926d4410ace
Florin Gabriel Abagiu <35663669+gaby911@users.noreply.github.com>
2021-02-27 23:10:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ showBtn.addEventListener('click',function(){
     for(let i = 0; i < showElements.length; i += 1) {
         showElements.item(i).classList.toggle("open");
     }
-
     if(showBtn.innerHTML === 'Hide<i class="fas fa-chevron-up"></i>'){
         showBtn.innerHTML = 'Show all<i class="fas fa-chevron-down"></i>';
     }else {
